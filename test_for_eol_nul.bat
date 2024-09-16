8020a4f23984f2a022850b971d137cc2508bb8d2
andry81 <andry@inbox.ru>
2024-09-16 14:58:26 +0300

    more delimiters

--- test_for_eol_nul.bat
+++ test_for_eol_nul.bat
@@ -5,4 +5,4 @@ setlocal
 rem ignored characters:
 rem 00 - NUL character
 rem 10 - LF character
-for /F usebackq^ tokens^=^*^ delims^=^ eol^= %%i in ("%~dp0eols.txt") do (echo.%%i)
+for /F "usebackq tokens=* delims="^ EOL^= %%i in ("%~dp0eols.txt") do (echo.%%i)
