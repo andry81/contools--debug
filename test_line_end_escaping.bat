e4d126c85247781a6b5c0661df8e562ceea65fe6
andry81 <andry@inbox.ru>
2025-03-06 04:06:40 +0300

    another line end escaping issue

--- test_line_end_escaping.bat
+++ test_line_end_escaping.bat
@@ -9,3 +9,18 @@ rem workaround
 cmd.exe /c echo ^
  "123" ^
 echo 456
+
+rem issue
+call; & ^
+"%COMSPEC%" /c echo 123 ^
+echo 456
+
+rem workaround 1
+call; & ^
+call; & "%COMSPEC%" /c echo 123 ^
+echo 456
+
+rem workaround 2
+call; ^
+ & "%COMSPEC%" /c echo 123 ^
+echo 456
