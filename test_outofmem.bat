68cd77acc7c2208c8ef28cce52698079df65cde7
andry81 <andry@inbox.ru>
2025-04-04 13:53:08 +0300

    links

--- test_outofmem.bat
+++ test_outofmem.bat
@@ -1,5 +1,9 @@
 @echo off
 
+rem Details:
+rem   https://stackoverflow.com/questions/15466298/simple-caret-at-end-of-windows-batch-file-consumes-all-memory
+rem   https://web.archive.org/web/20170814061717/https://stackoverflow.com/questions/23284131/cmd-exe-parsing-bug-leads-to-other-exploits
+
 call :OUT_OF_MEMORY ^^
 exit /b
 
