3865c4f957310394d30b30a5f5bdb7798d51e53f
andry81 <andry@inbox.ru>
2025-06-10 15:03:59 +0300

    typo

--- test_soft_ctrl_break_cmdexe_prompt_outter.bat
+++ test_soft_ctrl_break_cmdexe_prompt_outter.bat
@@ -3,7 +3,7 @@
 setlocal
 
 echo enter %~nx0
-echo warning: CTRL-BREAK won't exit outter prompt. You must type exit explicitly here!
+echo warning: CTRL-BREAK won't exit outer prompt. You must type exit explicitly here!
 cmd.exe /k @"%~dp0.%~n0\test.bat"
 echo exit %~nx0
 
