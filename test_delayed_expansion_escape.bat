675f5b0630394113865ac53610e85d8f93d0c89f
andry81 <andry@inbox.ru>
2026-01-07 09:08:31 +0300

    `!` character can be escaped while `ENABLEDELAYEDEXPANSION` is enabled

---
+++ test_delayed_expansion_escape.bat
@@ -0,0 +1,15 @@
+@echo off
+
+setlocal ENABLEDELAYEDEXPANSION
+
+set A=123
+
+echo;^!A^!A!A!
+
+setlocal DISABLEDELAYEDEXPANSION & for /F "usebackq tokens=* delims="eol^= %%i in ('"^!A^!A!A!"') do endlocal & echo;%%~i
+
+setlocal DISABLEDELAYEDEXPANSION & for /F "usebackq tokens=* delims="eol^= %%i in ('"^!A^!A!A!"') do endlocal & set "B=%%~i"
+set B
+
+setlocal DISABLEDELAYEDEXPANSION & for /F "usebackq tokens=* delims="eol^= %%i in ('"^!B^!B!B!"') do endlocal & echo;%%~i
+setlocal DISABLEDELAYEDEXPANSION & for /F "usebackq tokens=* delims="eol^= %%i in ('"!B!"') do endlocal & echo;%%~i
