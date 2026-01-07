932f0a03b56a36dc5bb5e025f4fc32c4a49d4e67
andry81 <andry@inbox.ru>
2026-01-07 10:19:03 +0300

    more details

--- test_delayed_expansion_escape.bat
+++ test_delayed_expansion_escape.bat
@@ -5,7 +5,9 @@ setlocal ENABLEDELAYEDEXPANSION
 set A=123
 
 echo;^!A^!A!A!
+echo;"^!A^!A!A!"
 
+setlocal DISABLEDELAYEDEXPANSION & for /F "usebackq tokens=* delims="eol^= %%i in ('^!A^!A!A!') do endlocal & echo;%%i
 setlocal DISABLEDELAYEDEXPANSION & for /F "usebackq tokens=* delims="eol^= %%i in ('"^!A^!A!A!"') do endlocal & echo;%%~i
 
 setlocal DISABLEDELAYEDEXPANSION & for /F "usebackq tokens=* delims="eol^= %%i in ('"^!A^!A!A!"') do endlocal & set "B=%%~i"
