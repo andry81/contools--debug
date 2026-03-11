5cbbe0afc00a0732292715eab66e8725011123af
andry81 <andry@inbox.ru>
2026-03-11 16:50:37 +0300

    tests on `Terminate Batch Job` skip

---
+++ test_terminate_batch_job_skip2.bat
@@ -0,0 +1,7 @@
+@ <nul 2>nul start /B /WAIT "" cmd.exe /c @start /WAIT "" cmd.exe /k @echo close me & exit /b
+
+rem 1. The inner `start /WAIT ""` opens a standalone console attached to the last child cmd.exe process.
+rem 2. The outer `start /B /WAIT ""` attaches the parent process console instead of open a new one.
+rem 3. The `<nul` is to avoid the user input await around the `Terminate Batch Job` message in case of a child console window close using UI.
+rem 4. The double start with wait hides the `Terminate Batch Job` message, but does not hide `^C` print into the parent console window.
+rem 5. The `2>nul start ...` suppresses `^C` prints into the parent console window.
---
+++ test_terminate_batch_job_skip5_spawn_tasks.bat
@@ -0,0 +1,17 @@
+@echo off
+
+setlocal
+
+call "%%~dp0__init__\__init__.bat" || exit /b
+
+set "CMDLINE_PREFIX=^<nul 2^>nul"
+
+call "%%CONTOOLS_ROOT%%/tasks/spawn_tasks.bat" 5 3 2 %%%%CMDLINE_PREFIX%%%% start /B /WAIT "" cmd.exe /c @start /WAIT "" cmd.exe /k @echo close me
+
+exit /b 0
+
+rem 1. The inner `start /WAIT ""` opens a standalone console attached to the last child cmd.exe process.
+rem 2. The outer `start /B /WAIT ""` attaches the parent process console instead of open a new one.
+rem 3. The `<nul` is to avoid the user input await around the `Terminate Batch Job` message in case of a console window close using UI.
+rem 4. The double start with wait hides the `Terminate Batch Job` message, but does not hide `^C` print into the parent console window.
+rem 5. The `2>nul start ...` suppresses `^C` prints into the parent console window, but the `call` prefix does bypass it, so we replaces `^<nul 2^>nul` to evaluate command line prefix after the call prefix.
