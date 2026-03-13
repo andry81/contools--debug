a5c9e9bad5e4fe54d43429293a31f5bf8f065b72
andry81 <andry@inbox.ru>
2026-03-13 17:48:35 +0300

    test `ERRORLEVEL` after `break`

--- test_for_concat.bat
+++ test_for_concat.bat
@@ -6,10 +6,22 @@ rem `break` as `noop`
 
 call;
 
+break
+
+echo 1.ERRORLEVEL=%ERRORLEVEL%
+
+(call)
+
+break
+
+echo 2.ERRORLEVEL=%ERRORLEVEL%
+
+call;
+
 (
   for %%i in (:) do for %%i in (:) do break ^
   & for %%i in (:) do for %%i in (:) do break ^
   & for %%i in (:) do for %%i in (:) do break
 )
 
-echo ERRORLEVEL=%ERRORLEVEL%
+echo 3.ERRORLEVEL=%ERRORLEVEL%
