de9322801d10d15a7dd1daef63c8d19160a5ce86
andry81 <andry@inbox.ru>
2026-03-17 11:48:41 +0300

    tests on variable expansion

---
+++ test_expand_delayed_var.bat
@@ -0,0 +1,35 @@
+@echo off
+
+setlocal DISABLEDELAYEDEXPANSION
+
+set ?.=^^^<nul 2^^^>nul start /B /WAIT "" cmd.exe /c @start /WAIT "" cmd.exe /k @echo close me
+
+set ?.
+
+echo ---
+
+setlocal ENABLEDELAYEDEXPANSION
+
+echo on
+
+@echo 1. exe cmdline expansion
+
+"!COMSPEC!" /c @echo_va_args.bat !?.!
+
+@echo ---
+
+@echo 2. start+exe cmdline expansion with old environment
+
+start /I /B /WAIT "" "!COMSPEC!" /c @echo_va_args.bat !?.!
+
+@echo ---
+
+@echo 3. call+script expansion
+
+call echo_va_args.bat !?.!
+
+@echo ---
+
+@echo 4. script w/o call expansion
+
+echo_va_args.bat !?.!
