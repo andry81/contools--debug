b69d31fafd4a3f4a3a5fd1c39d2e3f7805b04562
andry81 <andry@inbox.ru>
2026-03-23 06:52:12 +0300

    test using `start ... /WAIT` prefix

---
+++ test_skip_terminate_batch_job_by_filter_exit_code_4.bat
@@ -0,0 +1,9 @@
+@echo off
+
+setlocal
+
+set CMD_SKIP_TERMINATE_BATCH_JOB="%%SystemRoot%%\System32\cmd.exe" /S /c "@if %%ERRORLEVEL%% EQU -1073741510 (exit 9009) else exit %%ERRORLEVEL%%"
+
+start /WAIT "" timeout.exe /T -1 /NOBREAK & call %CMD_SKIP_TERMINATE_BATCH_JOB%
+
+echo ERRORLEVEL=%ERRORLEVEL%
