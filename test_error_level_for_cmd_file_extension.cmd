ad16ab6698beae382234f60d8e9c2051aff49535
andry81 <andry@inbox.ru>
2026-04-30 10:16:12 +0300

    rework of script file extension tests vs `echo` and `set` commands

--- test_error_level_for_cmd_file_extension.cmd
+++ test_error_level_for_cmd_file_extension.cmd
@@ -1,29 +1,90 @@
 @echo off
 
+rem NOTE: `setlocal DISABLEEXTENSIONS` is not tested
+
 setlocal
 
-rem sets error level
+rem echo pre tests at first
+echo --- `echo` pre tests ---
+
+call;
+echo ERRORLEVEL=%ERRORLEVEL%
+echo ERRORLEVEL=%ERRORLEVEL%
+if %ERRORLEVEL% EQU 0 ( echo - PASSED. ) else echo - FAILED.
+
 (call)
-echo 1. ERRORLEVEL=%ERRORLEVEL%
+echo ERRORLEVEL=%ERRORLEVEL%
+echo ERRORLEVEL=%ERRORLEVEL%
+if %ERRORLEVEL% EQU 1 ( echo - PASSED. ) else echo - FAILED.
 
-rem resets error level
+echo ---
+
+echo --- `set` tests on empty/not empty value ---
+
+rem 1.1. ERRORLEVEL=0
+call;
 set "A=123"
-echo 2. ERRORLEVEL=%ERRORLEVEL%
+(
+  if %ERRORLEVEL% EQU 0 ( <nul set /P "=PASSED: " ) else <nul set /P "=FAILED: "
+  echo 1.1. ERRORLEVEL=`0-^>%ERRORLEVEL%` EXPR=`set "A=123"`
+)
 
-setlocal ENABLEEXTENSIONS
-rem resets error level
-set "A=123"
-echo 3. ERRORLEVEL=%ERRORLEVEL%
-endlocal
-
-rem sets error level
+rem 1.2. ERRORLEVEL=0
 (call)
-echo 4. ERRORLEVEL=%ERRORLEVEL%
+set "A=123"
+(
+  if %ERRORLEVEL% EQU 0 ( <nul set /P "=PASSED: " ) else <nul set /P "=FAILED: "
+  echo 1.2. ERRORLEVEL=`1-^>%ERRORLEVEL%` EXPR=`set "A=123"`
+)
 
-rem resets error level
+rem 2.1. ERRORLEVEL=0
+call;
 set "A="
-echo 5. ERRORLEVEL=%ERRORLEVEL%
+(
+  if %ERRORLEVEL% EQU 0 ( <nul set /P "=PASSED: " ) else <nul set /P "=FAILED: "
+  echo 2.1. ERRORLEVEL=`0-^>%ERRORLEVEL%` EXPR=`set "A="`
+)
 
-rem sets error level
+rem 2.2. ERRORLEVEL=0
+(call)
+set "A="
+(
+  if %ERRORLEVEL% EQU 0 ( <nul set /P "=PASSED: " ) else <nul set /P "=FAILED: "
+  echo 2.2. ERRORLEVEL=`1-^>%ERRORLEVEL%` EXPR=`set "A="`
+)
+
+rem 3.1. ERRORLEVEL=1
+call;
+<nul set /P "A=123"
+echo;
+(
+  if %ERRORLEVEL% EQU 1 ( <nul set /P "=PASSED: " ) else <nul set /P "=FAILED: "
+  echo 3.1. ERRORLEVEL=`0-^>%ERRORLEVEL%` EXPR=`^<nul set /P "A=123"`
+)
+
+rem 3.2. ERRORLEVEL=1
+(call)
+<nul set /P "A=123"
+echo;
+(
+  if %ERRORLEVEL% EQU 1 ( <nul set /P "=PASSED: " ) else <nul set /P "=FAILED: "
+  echo 3.2. ERRORLEVEL=`1-^>%ERRORLEVEL%` EXPR=`^<nul set /P "A=123"`
+)
+
+rem 4.1. ERRORLEVEL=1
+call;
 <nul set /P "A="
-echo 6. ERRORLEVEL=%ERRORLEVEL%
+(
+  if %ERRORLEVEL% EQU 1 ( <nul set /P "=PASSED: " ) else <nul set /P "=FAILED: "
+  echo 4.1. ERRORLEVEL=`0-^>%ERRORLEVEL%` EXPR=`^<nul set /P "A="`
+)
+
+rem 4.2. ERRORLEVEL=1
+(call)
+<nul set /P "A="
+(
+  if %ERRORLEVEL% EQU 1 ( <nul set /P "=PASSED: " ) else <nul set /P "=FAILED: "
+  echo 4.2. ERRORLEVEL=`1-^>%ERRORLEVEL%` EXPR=`^<nul set /P "A="`
+)
+
+echo ---
