6e611a6ffb068d4f7a12e695dbceaeceaf0e56d2
andry81 <andry@inbox.ru>
2026-05-02 14:03:19 +0300

    install script fixup

--- _install.bat
+++ _install.bat
@@ -2,7 +2,13 @@
 
 setlocal
 
-call "%%~dp0__init__\__init__.bat" || exit /b
+rem CAUTION:
+rem   Immediate Administrator permissions is required to pass a root project
+rem   parameters into promoted environment of an external project script.
+rem   So you must elevate now to avoid the elevation later.
+
+call "%%~dp0._install/script_init.bat" %%0 %%* || exit /b
+if %IMPL_MODE%0 EQU 0 exit /b
 
 rem contools installation
 call "%%CONTOOLS_PROJECT_ROOT%%\_install.bat" %%*
